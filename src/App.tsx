aff891973bf9c406983a9056371d6bafbd116bcf
KowsalyaT06 <kowsalyat@dorustree.in>
2022-04-28 20:44:48 +0530

    typescript todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, FC, useState } from "react";
+import "./App.css";
 
-function App() {
+const App: FC = () => {
+  const [task, setTask]=useState<string>("")
+  const [days, setDays]=useState<number>(0)
+  const [todo, setTodo]=useState([])
+
+  const handleChange=(event:ChangeEvent<HTMLInputElement>)=>{
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="inputf">
+          <input type="text" placeholder="Task" onChange={handleChange}/>
+          <input type="number" placeholder="Days" onChange={handleChange}/>
+        </div>
+        <button>ADD TASK</button>
+      </div>
+      <div className="todoList"></div>
     </div>
   );
-}
-
+};
 export default App;
