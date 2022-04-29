c4952624e03eb6c759ed670a4d636fe2116799b4
KowsalyaT06 <kowsalyat@dorustree.in>
2022-04-29 13:15:26 +0530

    delete todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
 import React, { ChangeEvent, FC, useState } from "react";
 import "./App.css";
-
+import { Itask } from "./Interface";
+import Todo from "./components/Todo";
 const App: FC = () => {
-  const [task, setTask]=useState<string>("")
-  const [days, setDays]=useState<number>(0)
-  const [todo, setTodo]=useState([])
+  const [task, setTask] = useState<string>("");
+  const [days, setDays] = useState<number>(0);
+  const [todo, setTodo] = useState<Itask[]>([]);
 
-  const handleChange=(event:ChangeEvent<HTMLInputElement>)=>{
-
-  }
+  const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    if (e.target.name === "task") {
+      setTask(e.target.value);
+    } else {
+      setDays(Number(e.target.value));
+    }
+  };
+  const addTask = (): void => {
+    const newTask = { taskName: task, Days: days };
+    setTodo([...todo, newTask]);
+    setTask("");
+    setDays(0);
+    console.log("todo", todo);
+  };
+const deleteTask=(taskNameToDelete:string):void=>{
+setTodo(todo.filter((task)=>{
+  return task.taskName != taskNameToDelete
+}))
+}
   return (
     <div className="App">
       <div className="header">
         <div className="inputf">
-          <input type="text" placeholder="Task" onChange={handleChange}/>
-          <input type="number" placeholder="Days" onChange={handleChange}/>
+          <input
+            type="text"
+            placeholder="Task"
+            name="task"
+            value={task}
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            placeholder="Days"
+            name="days"
+            value={days}
+            onChange={handleChange}
+          />
         </div>
-        <button>ADD TASK</button>
+        <button onClick={addTask}>ADD TASK</button>
       </div>
-      <div className="todoList"></div>
+      <div className="todoList">
+        {todo.map((task:Itask, key:number) => {
+          return <Todo key={key} task={task} deleteTask={deleteTask}/>;
+        })}
+      </div>
     </div>
   );
 };
